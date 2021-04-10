77ed58b390e15dc93392d944b13e306b92da6de4
Aravind Venugopal <36126351+Aravind1444@users.noreply.github.com>
2021-04-10 16:26:07 +0530

    Initial commit

---
+++ app/src/main/java/me/aravind/sudotags/SplashScreen.java
@@ -0,0 +1,37 @@
+package me.aravind.sudotags;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //statubar color code
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#059669"));
+        //end of statusbar color code
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
+    }
+}
